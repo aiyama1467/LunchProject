2fdbcc1480655b6a2834bc64fc19fc97d1c436ef
Mizuki Sato <Aiyama2.718@gmail.com>
2019-11-26 14:58:26 +0900

    Miner change
    削除忘れのTodoを消した

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Todo: emailでログインできるようにする
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 from django.utils.translation import gettext_lazy as _
