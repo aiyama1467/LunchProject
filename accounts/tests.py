b54d646ed6510f6b999c7f7bf1fd968c97b34ec1
Mizuki Sato <Aiyama2.718@gmail.com>
2019-11-26 15:44:40 +0900

    テストの追加
    Sign upページへのアクセス
    アカウントの登録

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class PostSignupTests(TestCase):
+
+    def test_get(self):
+        """
+        getで通常のアクセスを行う.
+        """
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_new_account(self):
+        test_email = 'test@example.com'
+        test_password = 'warwetsrsrd12345'
+        user = User.objects.create(email=test_email, password=test_password)
+
+        response = self.client.get('/accounts/signup/')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, user.username)
+        # パスワードはハッシュで保存されている
