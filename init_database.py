10f178f34b00e2eff6d63a08739531c28715983e
Mizuki Sato <Aiyama2.718@gmail.com>
2019-12-03 11:44:20 +0900

    データベース初期化

---
+++ init_database.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+connection = sqlite3.connect('db.sqlite3')
+c = connection.cursor()
+
+allergies = ['卵', '乳', 'そば', '小麦', '落花生', 'エビ', 'かに',
+             'あわび', 'いか', 'いくら', 'さく', 'さば', 'くるみ',
+             'ゼラチン', '大豆', 'まつたけ', 'やまいも', '牛肉',
+             '豚肉', '鶏肉', 'オレンジ', 'キウイフルーツ', 'もも',
+             'バナナ', 'リンゴ', 'ゴマ', 'カシューナッツ']
+
+genres = ['和食', '洋食', '肉', '魚', '野菜', 'フルーツ',
+          'デザート', '麺', '丼', '主菜', '副菜', 'ごはん', '汁物']
+
+
+c.executemany('INSERT INTO accounts_allergies VALUES (?, ?)', enumerate(allergies))
+c.executemany('INSERT INTO accounts_genres VALUES (?, ?)', enumerate(genres))
+
+
+connection.commit()
+
+connection.close()
