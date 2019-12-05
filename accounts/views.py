6dc4fcb41686ef1fbcdc8769eafe536fb37d542a
Mizuki Sato <Aiyama2.718@gmail.com>
2019-12-06 00:03:03 +0900

    その日に食べた栄養素の合計を表示できるようにした

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import logout
 
+from collections import OrderedDict
+
 from accounts.models import User, EatLog
 from .forms import UserCreateForm
 
@@ -44,9 +46,49 @@ class UserMyPage(OnlyYouMixin, generic.DetailView):
     template_name = 'accounts/user_my_page.html'
     context_object_name = 'user'
 
+    class Data:
+        __slots__ = ['energy', 'carbohydrates', 'salt', 'fat', 'protein', 'red', 'green', 'yellow']
+
+        def __init__(self, menu=None):
+            if menu is not None:
+                self.energy = menu.menu_energy
+                self.carbohydrates = menu.menu_carbohydrate
+                self.salt = menu.menu_salt_content
+                self.fat = menu.menu_lipid
+                self.protein = menu.menu_protein
+                self.red = menu.menu_red_point
+                self.green = menu.menu_green_point
+                self.yellow = menu.menu_yellow_point
+            else:
+                self.energy = self.carbohydrates = self.salt = self.fat = self.protein = self.red = self.green = self.yellow = 0
+
+        def __add__(self, other):
+            self.energy += other.energy
+            self.carbohydrates += other.carbohydrates
+            self.salt += other.salt
+            self.fat += other.fat
+            self.protein += other.protein
+            self.red += other.red
+            self.green += other.green
+            self.yellow += other.yellow
+
+            return self
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['eat_log'] = EatLog.objects.filter(user=self.request.user)
+
+        eat_log = EatLog.objects.filter(user=self.request.user)
+        nutrient_shift = OrderedDict()
+
+        for log in eat_log:
+            if log.eat_datetime not in nutrient_shift.keys():
+                nutrient_shift[log.eat_datetime.isoformat()] = self.Data()
+
+            for m in log.menu.all():
+                nutrient_shift[log.eat_datetime.isoformat()] += self.Data(m)
+
+        context['nutrient_shift'] = nutrient_shift
+
         return context
 
 
