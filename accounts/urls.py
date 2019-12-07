e2d1adc22b9b7f2ebdfba8db401e309779df51b6
Mizuki Sato <Aiyama2.718@gmail.com>
2019-12-07 13:03:30 +0900

    パスワード変更ページを追加

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('my_page/', views.UserMyPage.as_view(), name='my_page'),
-    path('delete/', views.UserDeleteView.as_view(), name='delete')
+    path('delete/', views.UserDeleteView.as_view(), name='delete'),
+    path('password_modify/', views.PasswordModifyView.as_view(), name='password_modify'),
+    path('password_modify/done/', views.PasswordModifyDoneView.as_view(), name='password_modify_done'),
 ]
